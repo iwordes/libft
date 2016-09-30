9d77a2a72cc183f70b896209db042c4e9fbec6fb
Ian Wordes <iwordes@student.42.us.org>
2016-09-29 19:53:18 -0700

    Remove cruft

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -3,7 +3,7 @@ int			ft_strncmp(const char *string1, const char *string2, size_t n)
 {
 	if (string1 == string2)
 		return (0);
-	while ((t_byte)*string1 == (t_byte)*string2 && n-- != 0)
+	while (*string1 == *string2 && n-- != 0)
 	{
 		if (*string1 == 0)
 			return (0);
