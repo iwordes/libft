c43e5cc5b5260c5e1e1111394b0ee0d58348ecce
Ian Wordes <iwordes@student.42.us.org>
2016-10-02 17:04:14 -0700

    Normification: Stage 1

--- src/ft_struntil.c
+++ src/ft_struntil.c
@@ -1,4 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_struntil.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwordes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/02 16:45:10 by iwordes           #+#    #+#             */
+/*   Updated: 2016/10/02 16:50:57 by iwordes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
 /*
 ** Get the forward distance from the starting pointer *string* until the first
 ** instance of the character to match. If no match is found, it returns the
