32488ea040eb8c6b873cc2d89683d3c564784503
Ian Wordes <iwordes@student.42.us.org>
2016-10-29 10:16:32 -0700

    Complete ft_strssplit

---
+++ src/ft_strssplit.c
@@ -0,0 +1,69 @@
+#include "libft.h"
+
+/*
+** Splits a string by a given string delimiter and returns an array of
+** substrings.
+*/
+
+static int	get_substring_cnt(char *string, char *delim)
+{
+	size_t	cnt;
+	size_t	delim_length;
+
+	if (*delim == 0)
+		return (1);
+	cnt = 0;
+	delim_length = ft_strlen(delim);
+	while (*string != 0)
+	{
+		if (ft_strnequ(string, delim, delim_length))
+		{
+			string += delim_length;
+			while (ft_strnequ(string, delim, delim_length))
+				string += delim_length;
+			cnt++;
+		}
+		else
+			string++;
+	}
+	return (cnt);
+}
+
+static char	**panic(char **substrings)
+{
+	while (*substrings)
+	{
+		free(*substrings);
+		substrings++;
+	}
+	return (NULL);
+}
+
+char		**ft_strssplit(const char *string, const char *delim)
+{
+	char	**substr;
+	size_t	substring_cnt;
+	size_t	dl;
+	size_t	i;
+	size_t	s;
+
+	if (string == NULL || delim == NULL)
+		return (NULL);
+	i = 0;
+	s = 0;
+	dl = ft_strlen(delim);
+	substring_cnt = get_substring_cnt(string, delim);
+	NULL_GUARD(substr = malloc(sizeof(char*) * (substring_cnt + 1)));
+	while (s < substring_cnt)
+	{
+		while (string[i] != 0 && ft_strnequ(string + i, delim, dl))
+			i += dl;
+		if (string[i] != 0 && (substr[s]
+			= ft_strsub(string, i++, ft_struntils(string, delim))) != NULL)
+			return (panic(substr));
+		i += ft_struntils(string + i, delim);
+		s++;
+	}
+	substr[s] = NULL;
+	return (substr);
+}
