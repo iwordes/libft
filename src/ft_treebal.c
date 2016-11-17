29ab83c7303558b584a321781dbfc0815c5d0295
Ian Wordes <iwordes@student.42.us.org>
2016-11-17 15:38:31 -0800

    Stub out ft_treebal (incomplete)

--- src/ft_treebal.c
+++ src/ft_treebal.c
@@ -6,16 +6,20 @@
 /*   By: iwordes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 10:54:01 by iwordes           #+#    #+#             */
-/*   Updated: 2016/11/16 10:54:02 by iwordes          ###   ########.fr       */
+/*   Updated: 2016/11/17 15:37:52 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tree.h"
 
+/*
+** TODO: Balance the tree.
+*/
+
 void	ft_treebal(t_tree *tree, int (*cmp)(void*, void*))
 {
 	if (tree != NULL && cmp != NULL)
 	{
-		// Balance the tree.
+		(void)NULL;
 	}
 }
