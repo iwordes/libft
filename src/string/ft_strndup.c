8780ede1c791637a6681f3e4de3754c57d6eda14
Ian Wordes <iwordes@student.42.us.org>
2016-12-31 08:50:20 -0800

    Add ft_strndup; Update strdup to use size_t

--- src/string/ft_strndup.c
+++ src/string/ft_strndup.c
@@ -1,29 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/02 16:44:17 by iwordes           #+#    #+#             */
-/*   Updated: 2016/12/31 08:47:01 by iwordes          ###   ########.fr       */
+/*   Created: 2016/12/31 08:43:23 by iwordes           #+#    #+#             */
+/*   Updated: 2016/12/31 08:48:08 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char		*ft_strdup(const char *original)
+/*
+** Duplicate up to N characters of a string and return the copy.
+*/
+
+char	*ft_strndup(const char *string, size_t limit)
 {
 	char	*copy;
 	size_t	i;
 
 	i = 0;
-	copy = ft_strnew(ft_strlen(original));
+	copy = ft_strnew(MIN(ft_strlen(original), limit));
 	if (copy != NULL)
-		while (original[i])
+		while (original[i] != 0 && i < limit)
 		{
 			copy[i] = original[i];
-			i++;
+			i += 1;
 		}
 	return (copy);
 }
