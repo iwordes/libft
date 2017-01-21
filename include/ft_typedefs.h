91f02475a0e343dccdd9c2f86da58946ccfe18c0
Ian Wordes <iwordes@student.42.us.org>
2017-01-21 13:55:26 -0800

    Add single types to ft_typedefs.h: f32, f64, i8, i16, i32, i64, u8, u16, u32, u64

--- include/ft_typedefs.h
+++ include/ft_typedefs.h
@@ -6,12 +6,25 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 16:38:54 by iwordes           #+#    #+#             */
-/*   Updated: 2017/01/20 19:52:32 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/01/21 13:52:44 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_TYPEDEFS_H
 # define FT_TYPEDEFS_H
+
+typedef float			t_f32;
+typedef double			t_f64;
+
+typedef int8_t			t_i8;
+typedef int16_t			t_i16;
+typedef int32_t			t_i32;
+typedef int64_t			t_i64;
+
+typedef uint8_t			t_u8;
+typedef uint16_t		t_u16;
+typedef uint32_t		t_u32;
+typedef uint64_t		t_u64;
 
 typedef struct			s_2f32
 {
