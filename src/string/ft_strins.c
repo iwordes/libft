00fb6744800bd841cd58be7b8f716a09f98bfa5c
Ian Wordes <iwordes@student.42.us.org>
2017-02-02 13:25:26 -0800

    Improve ft_strins to do nothing when the given substring is empty

--- src/string/ft_strins.c
+++ src/string/ft_strins.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/02 11:37:43 by iwordes           #+#    #+#             */
-/*   Updated: 2017/02/02 11:51:46 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/02/02 13:24:54 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ char	*ft_strins(char *str, const char *substr, size_t st)
 
 	i = ft_strlen(str);
 	sl = ft_strlen(substr);
+	if (sl == 0)
+		return (str);
 	str[i-- + sl] = 0;
 	while (i > st)
 	{
